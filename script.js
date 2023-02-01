77413ef4e5565a5a8af276584ed49057d7574a39
mhertovm <mhertovmasyan904@gmail.com>
2023-02-01 17:24:45 +0400

    change

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const inputValu = document.querySelector("#inputValue")
 const btn = document.getElementById("add")
 const data = []
 btn.addEventListener("click", function(){
-    data.push(inputValu.value)
-    console.log(data[0][0]);
-    console.log(data[0][1]);
-});
+    data.push(+inputValu.value)
+    let sum = 0
+    for(let i = 0; i < data.length; i++){
+        sum = sum + data[i]
+    }
+    console.log(sum)
+    console.log(data[0])
+}); 
