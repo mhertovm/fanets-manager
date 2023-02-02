c981a6913d533511472af31ba4257f43c55d7b01
mhertovm <mhertovmasyan904@gmail.com>
2023-02-02 16:57:39 +0400

    newchange

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const inputValu = document.querySelector("#inputValue")
-const btnexpense = document.getElementById("aexpenseadd")
+const btnexpense = document.getElementById("expenseadd")
 const salary = document.querySelector("#salary")
 const quantity = document.getElementById("quantity")
-
+const expenseinput = document.getElementById("expenseinput")
 const data = []
 btnexpense.addEventListener("click", function(){
     data.push(+inputValu.value)
-    let sum = 0
+    let expensesum = 0
     for(let i = 0; i < data.length; i++){
-        sum = sum + data[i]
-        console.log(sum)
+        expensesum = expensesum + data[i]
         console.log(data[0])
-    }
-    quantity.textContent=salary.value-sum
+    };
+    expenseinput.textContent = expensesum
+    quantity.textContent=salary.value-expensesum
 }); 
 
