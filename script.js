31ee0117c96f34184298af8e49042c5364dadca5
mhertovm <mhertovmasyan904@gmail.com>
2023-02-05 22:02:50 +0400

    funct

--- script.js
+++ script.js
@@ -18,48 +18,49 @@ const mutq1 = document.getElementById("mutq1");
 const mutq2 = document.getElementById("mutq2");
 const mutq3 = document.getElementById("mutq3");
 const mutq4 = document.getElementById("mutq4");
+const sumall = document.getElementById("sum");
 let time = new Date();
 
 const cankData = [];
 const caxsdata = [];
 const balanceData = [];
 
-btnsalary.addEventListener("click", function(){
-
+btnsalary.addEventListener("click", function (){
+    
     if(salary.value > 0){
         balanceData.push(+salary.value)
     };
-
     let balance = 0
+    let expensesum = 0
+    
     for(let n = 0; n < balanceData.length; n++){
         balance = balance + balanceData[n];
         console.log(balanceData[0]);
     };
 
-    quantity.textContent = +balance + " AMD";
-});
-
-
-
-btnexpense.addEventListener("click", function(){
     if(inputValu.value > 0){
         caxsdata.push(+inputValu.value)
     };
-      
-    let expensesum = 0
+    
     for(let i = 0; i < caxsdata.length; i++){
         expensesum = expensesum + caxsdata[i];
         console.log(caxsdata[0]);
     };
-    expenseinput.textContent = expensesum + " AMD";
-});
-
-btnexpense.addEventListener("click", function(){
-    for(let j = 0; j < caxsdata.length; j++){
+    expenseinput.textContent = expensesum;
+    
+    for(let i = 0; i < caxsdata.length; i++){
         cankData.push(document.createElement("span"));
-        cank.append(cankData[j]);
-        cankData[j].textContent =time + "-" + caxsdata[j] + " AMD";    
+        cank.append(cankData[i]);
+        cankData[i].textContent =time + "-" + caxsdata[i] + " AMD";    
     }
+
+    let sum = balance - expensesum
+    sumall.textContent = sum
+    quantity.textContent = balance
+    mutq1.textContent = qanak1.textContent * sum / 100
+    mutq2.textContent = qanak2.textContent * sum / 100
+    mutq3.textContent = qanak3.textContent * sum / 100
+    mutq4.textContent = qanak4.textContent * sum / 100
 });
 
 
@@ -71,4 +72,5 @@ btnedit.addEventListener("click", function(){
     qanak3.textContent = +edit3.value;
     qanak4.textContent = +edit4.value;
     }
+    btnsalary.addEventListener("click")
 });
